ef08a1aa6017260ff2b31566a1f7407d4cf4f247
John Weis <weis.john@gmail.com>
2015-12-02 15:21:23 -0500

    rewriting cache rule to use common fetcher

--- rules/cache.js
+++ rules/cache.js
@@ -1,37 +1,29 @@
 
 // evaluate if there is a cache breaking system
 
-var request = require('request');
-var cheerio = require('cheerio');
 var debug = require('debug')('best:cache');
+var utils = require('./utils');
 
 // cache breaking detection regexes
 var hashed = /[0-9a-f]{4}/;
 var versioned = /\?(v)\=/;
 
+// we need CSS and JS files
+var tags = [{
+  selector: 'link[rel=stylesheet]',
+  attr: 'href',
+}, {
+  selector: 'script',
+  attr: 'src',
+}];
+
 module.exports = function cache(config, cb) {
-  request.get(config.url, function(err, res) {
-    if (err) return cb(err);
 
-    // parse the HTML for resources
-    var $ = cheerio.load(res.body);
-    var resources = [];
+  config.tags = tags;
+  utils.resources(config, function(err, results) {
+    if (err || !results) return cb(err);
 
-    $('link[rel=stylesheet]').each(function() {
-      var href = $(this).attr('href');
-      if (href) resources.push(href);
-    });
-
-    $('script').each(function() {
-      var src = $(this).attr('src');
-      if (src) resources.push(src);
-    });
-
-    // ignore different domains & protocol relative resources
-    var locals = resources.filter(function(resource) {
-      if (!/^(http\:|https\:)?\/\//.test(resource)) return true;
-      debug('ignore', resource);
-    });
+    var locals = results.locals;
 
     // detect whether or not it uses a cache breaker
     var cached = locals.filter(function(resource) {
@@ -53,7 +45,7 @@ module.exports = function cache(config, cb) {
     // determine score
     var response = {
       pass: (locals.length / 2) < cached.length,
-      errors: locals.length - cached.length
+      errors: locals.length - cached.length,
     };
 
     cb(null, response);
