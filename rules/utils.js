81fe17994c2c1a0246aa294ec5757335a7ccf2f5
John Weis <weis.john@gmail.com>
2015-12-02 15:41:21 -0500

    fix linting errors in utils

--- rules/utils.js
+++ rules/utils.js
@@ -13,28 +13,28 @@ function resources(config, cb) {
 
     // parse the HTML for resources
     var $ = cheerio.load(res.body);
-    var resources = [];
+    var _resources = [];
 
     // find elements which match the tags specified by caller
     config.tags.forEach(function(tag) {
       $(tag.selector).each(function() {
         var attr = $(this).attr(tag.attr);
-        if (attr) resources.push(attr);
+        if (attr) _resources.push(attr);
       });
     });
 
-    // ignore different domains & protocol relative resources
-    var locals = resources.filter(function(resource) {
+    // ignore different domains & protocol relative _resources
+    var locals = _resources.filter(function(resource) {
       if (!/^(http\:|https\:)?\/\//.test(resource)) return true;
       debug('ignore', resource);
     });
 
-    debug('totals ' + resources.length);
+    debug('totals ' + _resources.length);
     debug('assets ' + locals.length);
 
-    cb(null, { resources: resources, locals: locals });
+    cb(null, { resources: _resources, locals: locals });
 
   });
-};
+}
 
 module.exports = { resources: resources };
