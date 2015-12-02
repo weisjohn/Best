22e7a4eb91a227691238b271638d816288aff438
John Weis <weis.john@gmail.com>
2015-12-02 16:40:52 -0500

    adding gzip rule

---
+++ rules/gzip.js
@@ -0,0 +1,58 @@
+
+// detect if resources are sent using gzip
+
+var request = require('request');
+var async = require('async');
+var url = require('url');
+var _ = require('lodash');
+
+var debug = require('debug')('best:gzip');
+var utils = require('./utils');
+
+// we need CSS and JS files
+var tags = [{
+  selector: 'link[rel=stylesheet]',
+  attr: 'href',
+}, {
+  selector: 'script',
+  attr: 'src',
+}];
+
+module.exports = function favicon(config, cb) {
+
+  config.tags = tags;
+  utils.resources(config, function(err, res) {
+    if (err || !res || !res.resources) return cb(err);
+
+    // fetch each resource
+    async.map(res.resources, function(resource, _cb) {
+
+      var _url = url.resolve(config.url, resource);
+      debug('fetch ' + resource);
+
+      // fetch the resource
+      request.get({ url: _url, gzip: true }, function(_err, _res) {
+        if (_err) return cb(_err);
+
+        var encoding = _res.headers['content-encoding'];
+
+        if (/gzip/.test(encoding)) return _cb(null, resource);
+
+        return _cb();
+      });
+
+    }, function(err, result) {
+      if (err) return cb(err);
+
+      // remove empty items
+      result = _.without(result, undefined);
+
+      // all resources must be sent with gzip
+      var pass = result.length === res.resources.length;
+      var errors = _.difference(res.resources, result);
+
+      cb(null, { pass: pass, errors: errors });
+    });
+
+  });
+};
