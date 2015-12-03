3dca0b7c05f4e030dced041a841459f24254a02b
John Weis <weis.john@gmail.com>
2015-12-03 18:06:54 -0500

    adding memoized versions of request.get and utils.resources to help minimize fetching the same urls over and over

--- rules/utils.js
+++ rules/utils.js
@@ -3,40 +3,56 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
+var async = require('async');
+var sigmund = require('sigmund');
+
 var debug = require('debug')('best:util');
 
-// TODO: this should be memo-ized
-function resources(config, cb) {
-  if (!config.tags) throw new Error('A tag map must be specified');
+// a simple memoized wrapper around request
+var utils = {
+  get: async.memoize(function(opts, cb) {
+    debug('fetching: ' + opts.url);
+    return request.get(opts, cb);
+  }, function(opts) {
+    return sigmund(opts.url);
+  })
+};
 
-  // TODO: log the URL we're fetching
-  request.get(config.url, function(err, res) {
+// return an array of values from a tag map (an array of objects with selector,property pairs)
+function _resources(config, cb) {
+  if (!config.tags) throw new Error('A tag map must be specified');
+  utils.get(config, function(err, res) {
     if (err) return cb(err);
 
     // parse the HTML for resources
     var $ = cheerio.load(res.body);
-    var _resources = [];
+    var resources = [];
 
     // find elements which match the tags specified by caller
     config.tags.forEach(function(tag) {
       $(tag.selector).each(function() {
         var attr = $(this).attr(tag.attr);
-        if (attr) _resources.push(attr);
+        if (attr) resources.push(attr);
       });
     });
 
-    // ignore different domains & protocol relative _resources
-    var locals = _resources.filter(function(resource) {
+    // ignore different domains & protocol relative resources
+    var locals = resources.filter(function(resource) {
       if (!/^(http\:|https\:)?\/\//.test(resource)) return true;
       debug('remote', resource);
     });
 
-    debug('totals ' + _resources.length);
+    debug('totals ' + resources.length);
     debug('assets ' + locals.length);
 
-    cb(null, { resources: _resources, locals: locals });
+    cb(null, { resources: resources, locals: locals });
 
   });
 }
 
-module.exports = { resources: resources };
+// memoize with a custom hash - https://github.com/caolan/async#memoize
+utils.resources = async.memoize(_resources, function(config) {
+  return sigmund({ url: config.url, tags: config.tags });
+});
+
+module.exports = utils;
