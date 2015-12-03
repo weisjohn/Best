0436626d56c3445affc1e1cfe0e7f435798fc9c3
John Weis <weis.john@gmail.com>
2015-12-03 04:33:32 -0500

    add resources rule, fail if more than 10

---
+++ rules/resources.js
@@ -0,0 +1,31 @@
+
+// detect if there are more than 10 resources
+
+var debug = require('debug')('best:resources');
+var utils = require('./utils');
+
+// we need CSS and JS files
+var tags = [{
+  selector: 'link[rel=stylesheet]',
+  attr: 'href',
+}, {
+  selector: 'script',
+  attr: 'src',
+}];
+
+var max = 10;
+
+module.exports = function resources(config, cb) {
+
+  config.tags = tags;
+  utils.resources(config, function(err, results) {
+    if (err) return cb(err);
+
+    var len = results.resources.length;
+    var ret = { pass: len < max };
+    if (!ret.pass) ret.errors = results.resources;
+
+    cb(null, ret);
+
+  });
+};
